25b5e4ecc94db27bb4aaaa05caaeb989bdba48a5
root <root@kali.kali>
2020-04-03 15:12:44 +0300

    lets test this out

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-var l = 88;
+var ifrm1 = document.createElement("iframe");
+ifrm1.setAttribute("src", "https://www.youtube.com/embed/irAJuT0TrqQ");
+ifrm1.setAttribute("width", "420");
+ifrm1.setAttribute("height", "315");
+ifrm1.setAttribute("allowFullScreen", "");
+var ifrm2 = document.createElement("iframe");
+ifrm2.setAttribute("src", "https://www.youtube.com/embed/xu3BljsP5Qk");
+ifrm2.setAttribute("width", "420");
+ifrm2.setAttribute("height", "315");
+ifrm2.setAttribute("allowFullScreen", "");
+
+var ele = document.getElementById("marker");
+ele.appendChild(ifrm1);
+ele.appendChild(ifrm2);
